604a7fc9e226531b591093485ff84633bc26e10c
Sabari <r.sabarinathan22@gmail.com>
2022-01-15 13:34:28 +0530

    first commit

---
+++ src/com/sabari/game/Main.java
@@ -0,0 +1,35 @@
+package com.sabari.game;
+
+
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+
+    public Main() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setResizable(false);
+        pack();
+
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Main();
+            ex.setVisible(true);
+        });
+    }
+}
